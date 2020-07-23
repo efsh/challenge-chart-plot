18d04488a45d4802a78635c4d2e7ea1786b072c2
Eduardo Henriques <edufsh@gmail.com>
2020-07-23 16:44:44 -0300

    created basic controls

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import React, {Component} from 'react'
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, React!</h1>
-      </div>
-    )
-  }
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleClick(event) {
+        console.log(this.state.value);
+        event.preventDefault();
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div className="input-data">
+                    <textarea
+                        onChange={this.handleChange}
+                        value={this.state.value}
+                        cols="30" 
+                        rows="10" />
+                </div>
+                <div className="chart">
+                    <p>-- chart -- </p>
+                </div>
+                <div className="execute">
+                <input
+                    type="button"
+                    value="GENERATE CHART"
+                    onClick={this.handleClick} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App
